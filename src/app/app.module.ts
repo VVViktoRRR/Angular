99940e377f7c825d5278659ad6286970a05b4f26
VVViktoRRR <viktorusenko1604@gmail.com>
2022-05-13 14:04:25 +0300

    Angular_Lesson_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { UsersComponent } from './users/users.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent,
+    CommentsComponent,
+    CommentComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
